d3b3177f2d053d3226b66c7e10cd60248587f42d
andrew <phoodnotebook@gmail.com>
2020-04-23 17:51:35 -0700

    First Update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"Testing the post  ","categories": ["Jekyll","test"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/test/test/",
+        "teaser": null
+      }]
